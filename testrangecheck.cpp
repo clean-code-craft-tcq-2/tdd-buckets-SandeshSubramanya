f8c900331a34abf84952a73357cd9da56e652962
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-08 16:28:39 +0530

    Update testrangecheck.cpp

--- testrangecheck.cpp
+++ testrangecheck.cpp
@@ -1,9 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "test/catch.hpp"
+#include <iostream>
+#include <string>
+#include <map>
 #include <vector>
 using namespace std;
 
+typedef std::pair<int,int> RangePair;
+typedef std::map<RangePair, int> SingleRangePairResult; // type to store result corresponding to a particular range.
+typedef std::vector<SingleRangePairResult> oRangeCheckResults;  
+
 TEST_CASE(" Test Smallest Possible range") 
 {
-  
+  // test the code intially with the smallest range pair.
+  RangePair oPair(4, 5);  
+  SingleRangePairResult oNumbersInRanges;
+  oNumbersInRanges.insert({oPair, 2});
+  oRangeCheckResults oExpectedResult {oNumbersInRanges};
 }
