111f7f905960f96cef6b9b8c7d3f10f88ddc1c0e
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-08 19:06:38 +0530

    Update rangecheck.h

--- rangecheck.h
+++ rangecheck.h
@@ -1,1 +1,10 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <vector>
+typedef std::pair<int,int> RangePair;
+typedef std::map<RangePair, int> SingleRangePairResult; // type to store result corresponding to a particular range.
+typedef std::vector<SingleRangePairResult> oRangeCheckResults;  
+
+oRangeCheckResults DetermineRange(vector <int> &rfInputValues);
 
