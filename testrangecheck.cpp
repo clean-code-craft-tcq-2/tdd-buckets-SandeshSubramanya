8bd26afc2b7a8b3b7d13bd0a151cfcafef3306f2
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-08 16:20:26 +0530

    Update testrangecheck.cpp

--- testrangecheck.cpp
+++ testrangecheck.cpp
@@ -1,7 +1,9 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "test/catch.hpp"
+#include <vector>
+using namespace std;
 
-TEST_CASE("Test Range") 
+TEST_CASE(" Test Smallest Possible range") 
 {
   
 }
