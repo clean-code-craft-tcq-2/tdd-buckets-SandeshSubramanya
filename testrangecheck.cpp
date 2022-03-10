a47023c8031db43a001da219dce3c0f45bcf051b
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-10 15:05:40 +0530

    Update testrangecheck.cpp

--- testrangecheck.cpp
+++ testrangecheck.cpp
@@ -3,17 +3,22 @@
 #include "rangecheck.h"
 using namespace std;
 
-
 TEST_CASE(" Test Smallest Possible range") 
 {
   // test the code intially with the smallest range pair.
   vector<int> oInputValues {4,5};
   oRangeCheckResults oCalculatedResults = CalculateReadingsAndRangeFromValues(oInputValues);
   int num_of_readings = 0;
+  int start_of_range = 0;
+  int end_of_range = 0;
   if(!oCalculatedResults.empty())
   {
     SingleRangePairResult oCalculatedNumbersInRange = oCalculatedResults[0];  // get the map info.
     num_of_readings = oCalculatedNumbersInRange.begin()->second;
+    end_of_range = oRangeCheckResultsTemp[0].begin()->first.first;
+    higher_end_of_range = oRangeCheckResultsTemp[0].begin()->first.second;
   }
-  assert(num_of_readings == 2); // number of elements in the range is expected to be 2.
+  assert(start_of_range == 4);
+  assert(end_of_range == 5);
+  assert(num_of_readings == 2);
 }
