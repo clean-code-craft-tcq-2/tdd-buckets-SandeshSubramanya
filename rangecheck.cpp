1ecc326696d4672ea9b3b63259c14b99ba580241
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-10 14:49:12 +0530

    Update rangecheck.cpp

--- rangecheck.cpp
+++ rangecheck.cpp
@@ -1,16 +1,46 @@
 #include "rangecheck.h"
 #include <algorithm>
 
+unsigned getIndexOfLastElementInContinousRange(unsigned startIndex, const std::vector<int> &rfInputValues, int &count)
+{
+    unsigned IndexOfLastElementInContinousRange = 0;
+    while (
+           (startIndex < rfInputValues.size())
+           &&
+           ((startIndex + 1) < rfInputValues.size())
+           )
+           {
+               if( (rfInputValues[startIndex+1] - rfInputValues[startIndex]) <= 1)
+               {
+                   IndexOfLastElementInContinousRange = startIndex + 1;
+                   startIndex = IndexOfLastElementInContinousRange;
+                   count++;
+               }
+           }
+    return IndexOfLastElementInContinousRange;
+}
+
 oRangeCheckResults CalculateReadingsAndRangeFromValues(std::vector<int> &rfInputValues)
 {
   // firstly sort the input values.
   std::sort(rfInputValues.begin(),rfInputValues.end());
-  
-  // for now, write a simple code to make simple test case pass. 
-  // Future find the logic to handle this.
   oRangeCheckResults oRangeCheckResultsTemp;  
   SingleRangePairResult oTotalNumbersInRanges;
-  oTotalNumbersInRanges.insert({{4, 5}, 2});
-  oRangeCheckResultsTemp.push_back(oTotalNumbersInRanges);
+  unsigned int indexOfLastElementInContinousRange = 0;
+  int numberOfElementsInCurrentRange =1;
+  for(unsigned startIndex = 0; startIndex < rfInputValues.size(); startIndex +=indexOfLastElementInContinousRange)
+  {
+      numberOfElementsInCurrentRange =1;
+      indexOfLastElementInContinousRange = getIndexOfLastElementInContinousRange(startIndex,rfInputValues, numberOfElementsInCurrentRange);
+      if(startIndex < rfInputValues.size() && indexOfLastElementInContinousRange < rfInputValues.size())
+      {
+          int firstElementInRange =  rfInputValues[startIndex];
+          int lastElementInRange =  rfInputValues[indexOfLastElementInContinousRange];
+          SingleRangePairResult oTotalNumbersInRanges;
+          oTotalNumbersInRanges.insert({{firstElementInRange, lastElementInRange}, numberOfElementsInCurrentRange});
+          oRangeCheckResultsTemp.push_back(oTotalNumbersInRanges);
+      }
+      ++indexOfLastElementInContinousRange;
+  }
   return oRangeCheckResultsTemp;
 }
