162d0774a931e7c601ea244d6d9945b06fc52b04
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-29 23:31:43 +0530

    Update a2dConverter.h

--- a2dConverter.h
+++ a2dConverter.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <string>
+
+typedef enum sensor_type
+{
+  EN_12_BIT_SENSOR = 0,
+  EN_10_BIT_SENSOR = 1
+}enSensorType;
+
+unsigned getMaxVoltage(enSensorType oSensorType)
+{
+  if(oSensorType == enSensorType::EN_12_BIT_SENSOR)
+    return 4094;
+  else
+    return 1023;
+}
