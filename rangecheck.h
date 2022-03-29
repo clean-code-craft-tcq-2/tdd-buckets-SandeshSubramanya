2c8fe5e2c815d0f39d84446bfd92b1d6c934299b
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-29 21:11:56 +0530

    Consider older review comment

--- rangecheck.h
+++ rangecheck.h
@@ -7,6 +7,6 @@ typedef std::pair<int,int> RangePair;
 typedef std::map<RangePair, int> SingleRangePairResult; // type to store result corresponding to a particular range.
 typedef std::vector<SingleRangePairResult> oRangeCheckResults;  
 
-unsigned getIndexOfLastElementInContinousRange(unsigned startIndex, const std::vector<int> &rfInputValues, int &count);
+unsigned getIndexOfLastElementInContinousRange(unsigned startIndex, const std::vector<int> &rfSortedInputValues, int &count);
 oRangeCheckResults CalculateReadingsAndRangeFromValues(std::vector<int> &rfInputValues);
 
