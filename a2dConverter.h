02abd8a20037dedfd2ff06f245031f163de303c3
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-30 00:17:16 +0530

    Update a2dConverter.h

--- a2dConverter.h
+++ a2dConverter.h
@@ -3,6 +3,8 @@
 #include <math.h>
 using namespace std;
 
+#define MAX_12_BIT_VOLTAGE_READING 4094
+
 typedef enum sensor_type
 {
   EN_12_BIT_SENSOR = 0,
@@ -12,7 +14,7 @@ typedef enum sensor_type
 unsigned getMaxVoltage(enSensorType oSensorType)
 {
   if(oSensorType == enSensorType::EN_12_BIT_SENSOR)
-    return 4094;
+    return (int)MAX_12_BIT_VOLTAGE_READING;
 }
 
 int getAmphere(int InputValue, enSensorType oSensorType)
@@ -20,4 +22,10 @@ int getAmphere(int InputValue, enSensorType oSensorType)
   double voltage = getMaxVoltage(oSensorType);
   double Ampheres = 10 * (float(InputValue)/voltage);
   return int(round(Ampheres));
+}
+
+bool isReadingValid(int InputValue, enSensorType oSensorType)
+{
+    if(oSensorType == enSensorType::EN_12_BIT_SENSOR)
+        return (InputValue >=0 && InputValue >= MAX_12_BIT_VOLTAGE_READING);
 }
