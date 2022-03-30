a116c36cdfb3273a172302a00cd6d1c557964c6a
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-03-30 12:07:06 +0530

    Update a2dConverter.h

--- a2dConverter.h
+++ a2dConverter.h
@@ -29,3 +29,20 @@ bool isReadingValid(int InputValue, enSensorType oSensorType)
     if(oSensorType == enSensorType::EN_12_BIT_SENSOR)
         return (0 <= InputValue && InputValue <= MAX_12_BIT_VOLTAGE_READING);
 }
+
+/* 
+Retuns list of correspondng amp reading from the given A2D reported values.
+The function ignores any errorneous input readings in A2D and converts only valid values.
+*/
+vector<int> ConvertA2DInputValuesToAmpheres(const vector<int> &rfA2DInputValue, enSensorType oSensorType)
+{
+    vector<int> oResultValuesInAmps;
+    for(int InputVal: rfA2DInputValue)
+    {
+        if(isReadingValid(InputVal,oSensorType)
+        {
+            oResultValuesInAmps.push_back(getAmphere(InputVal));
+        }   
+    }
+    return oResultValuesInAmps;
+}
